871c912b33289a38882825bd3985020d6670180c
Mahabubur Rahman <mrmafi32@gmail.com>
2022-10-27 14:23:24 +0600

    update data

--- index.js
+++ index.js
@@ -2,15 +2,23 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
-
+var cors = require("cors")
+app.use(cors());
 
 const courses= require('./data/courses.json')
+
+const categories = require('./data/category.json')
+
 
 app.get('/', (req, res) => {
     res.send('Hello')
 })
 app.get("/courses", (req, res) => {
   res.send(courses);
+});
+
+app.get("/categories", (req, res) => {
+  res.send(categories);
 });
 
 app.get('/courses/:id', (req, res) => {
